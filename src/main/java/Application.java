233c0957c33beb9a8871801cce50ac2c109f4043
BernardoMagalhaes13 <josebmjesus12@gmail.com>
2023-10-06 17:12:56 +0100

    Passo 7

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,20 +1,16 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
+
 public class Application {
-    public static void main(String[] args) {
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private int x = 10;
+    private int y = 10;
+    public static void main(String[] args) throws IOException {
+        new Game(40, 40).run();
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,20 +1,56 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
 public class Game {
-    public static void main(String[] args) {
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-        } catch (IOException e) {
-            e.printStackTrace();
+    private final TerminalScreen screen;
+    private int x = 10;
+    private int y = 10;
+    public Game(int  width, int height) throws IOException, IOException {
+        Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don’t need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+    }
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+    public void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+            screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
+    }
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+        String keyT = key.getKeyType().toString();
+        switch (keyT) {
+            case "ArrowUp":
+                y = y - 1;
+                break;
+            case "ArrowDown":
+                y = y + 1;
+                break;
+            case "ArrowLeft":
+                x = x - 1;
+                break;
+            case "ArrowRight":
+                x = x + 1;
+                break;
         }
     }
 }
